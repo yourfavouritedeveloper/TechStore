d0b18763d1f45f9a1ff6791098ade6c80e9346a3
nihadmammadov <nihad.mammadov.16@gmail.com>
2025-07-27 02:39:47 +0400

    Module ADDED

--- Frontend/src/Background/Background.jsx
+++ Frontend/src/Background/Background.jsx
@@ -1,34 +1,76 @@
 
 import styles from "./Background.module.css"
-import { motion, useInView, useAnimation } from 'framer-motion';
-import React, { useEffect, useRef, forwardRef } from 'react';
+import { motion, useInView, useAnimation } from "framer-motion";
+import React, { useEffect, useRef } from "react";
+import Module from "../Model/Module";
 
 function Background() {
-        const mainControls = useAnimation();
-        const localRef = useRef(null);
-        const isInView = useInView(localRef, { once: true });
-        useEffect(() => {
-            if (isInView) {
-                mainControls.start("visible");
-            }
-        }, [isInView]);
+  const boxRef = useRef(null);
+  const moduleRef = useRef(null);
+  const ref = useRef(null);
+
+  const boxControls = useAnimation();
+  const moduleControls = useAnimation();
+  const controls = useAnimation();
+
+  const boxInView = useInView(boxRef, { once: true });
+  const moduleInView = useInView(moduleRef, { once: true });
+  const inView = useInView(ref, { once: true });
+
+
+  useEffect(() => {
+    if (boxInView) boxControls.start("visible");
+    if (moduleInView) moduleControls.start("visible");
+    if (inView) controls.start("visible");
+  }, [boxInView, moduleInView,inView]);
     
     return (
         <>
         <div className={styles.container}>   
             <motion.div 
+            ref={boxRef}
             className={styles.box}
             variants={{
-                hidden: { y: "300px"},
+                hidden: { y: "280px"},
                 visible: { y: "0px" }
             }}
-            initial="hidden"
-            whileInView="visible"
             viewport={{ margin: "10px" }}
-            animate={mainControls}
-            transition={{ duration: 1.5 }}   
+            initial="hidden"
+            animate={boxControls}
+            transition={{ duration: 1.5,delay:2 }}   
             >
 
+            </motion.div>
+            <motion.div className={styles.module}
+            ref={moduleRef}
+            variants={{
+                hidden: { opacity:1},
+                visible: { opacity:1}
+            }}
+            initial="hidden"
+            viewport={{ margin: "10px" }}
+            animate={moduleControls}
+            transition={{ duration: 1.5 }} 
+                         
+            >
+                <Module />
+
+            <motion.div
+            ref={ref}
+            className={styles.fadeOverlay}
+            variants={{
+            hidden: { opacity: 1 },
+            visible: { opacity: 0 },
+            }}
+            initial="hidden"
+            animate={controls}
+            transition={{ duration:5,delay:2}}
+            />
+            </motion.div>
+            
+            <motion.div
+            className={styles.title}>
+                <p>Welcome</p>
             </motion.div>
         </div>
         
