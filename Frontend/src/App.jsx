428f3bd55bcb90b353ccd169ce5ca7257f6afde1
nihadmammadov <nihad.mammadov.16@gmail.com>
2025-07-26 20:48:50 +0400

    Nav and Background ADDED

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useEffect } from 'react'
+import Nav from './Nav/Nav'
+import Background from './Background/Background'
 import './App.css'
+import Module from './Model/Module'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+  useEffect(() => {
+    if ('scrollRestoration' in window.history) {
+      window.history.scrollRestoration = 'manual';
+    }
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Nav />
+      <Background />
+      <Module />
+
     </>
   )
 }
---
+++ Frontend/src/Model/Module.jsx
@@ -0,0 +1,30 @@
+import { OrbitControls ,PerspectiveCamera} from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Model } from "./Model";
+
+function Module() {
+
+    return(
+        <>
+        <Canvas
+        style={{
+            height: "100vh",
+            width: "100vw",
+            position: "absolute",
+            marginTop: "-145rem",
+            zIndex: "2"
+        }}>
+        <PerspectiveCamera makeDefault position={[-10, 9,10]} fov={0.8} />
+        <OrbitControls 
+        enableZoom={false} 
+        enablePan={false}/>
+        <ambientLight intensity={10} />
+        <directionalLight position={[-600, 100, -500]} intensity={6000} />
+        <pointLight position={[0, 5, 10]} intensity={0.8} />
+        <Model />
+        </Canvas>
+        </>
+    );
+}
+
+export default Module;
