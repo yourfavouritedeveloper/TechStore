8d873f1ab19e256998376b81a4e493920779f53f
nihadmammadov <nihad.mammadov.16@gmail.com>
2025-07-28 02:06:13 +0400

    Categories ADDED

--- Frontend/src/Body/Body.jsx
+++ Frontend/src/Body/Body.jsx
@@ -1,9 +1,42 @@
 import styles from "./Body.module.css"
+import Macbook from "../assets/Macbook.png"
+import Iphone from "../assets/iphone.png"
 
 function Body() {
     return(
         <>
-            <div className={styles.container}></div>
+            <div className={styles.container}>
+                <p className={styles.title}>Categories</p>
+                <ul className={styles.categories}>
+
+
+                    <li className={styles.computers}>
+                        <span className={styles.circle1}></span>
+                        <span className={styles.circle2}></span>
+                        <span className={styles.circle3}></span>    
+                        <span className={styles.circle4}></span>                        
+                        <p>Computers</p>
+                        <img src={Macbook} alt="Computers" />
+                        </li>
+
+
+
+                    <li className={styles.mobilePhones}>
+                        <span className={styles.circle1}></span>
+                        <span className={styles.circle2}></span>
+                        <span className={styles.circle3}></span>    
+                        <span className={styles.circle4}></span>                            
+                        <p>Phones</p>
+                        <img src={Iphone} alt="Phones" />
+                    </li>
+                    <li className={styles.tvs}>TVs</li>
+                    <li className={styles.smartWatches}>Smart Watches</li>
+                    <li className={styles.keyboards}>Keyboards</li>
+                    <li className={styles.headphones}>Headphones</li>
+                    <li className={styles.monitors}>Monitors</li>
+                    <li className={styles.tablets}>Tablets</li>
+                </ul>
+            </div>
         </>
 
     );
