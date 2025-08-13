6da38374cfcae5e467ebe2a3f8ff4f2680b8e0f9
nihadmammadov <nihad.mammadov.16@gmail.com>
2025-08-13 16:03:33 +0400

    Login page UPDATED

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,6 +5,7 @@ import About from "./Pages/About";
 import Items from "./Pages/Items";
 import Login from "./Pages/Login";
 import { AuthProvider } from "./Components/AuthContext";
+import Register from "./Pages/Register";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/product" element={<Items />}/>
           <Route path="/about" element={<About />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </HashRouter>
     </AuthProvider>
