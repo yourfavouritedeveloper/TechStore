8890afb68d69a1df6ffaa359e4211e125d0ce8b8
nihadmammadov <nihad.mammadov.16@gmail.com>
2025-08-25 01:59:19 +0400

    Product buying page ADDED

--- Frontend/src/Components/Item/Item.jsx
+++ Frontend/src/Components/Item/Item.jsx
@@ -7,7 +7,7 @@ function Item({name}) {
     const [item,setItem] = useState([])
 
     useEffect(() => {
-    axios.get(`https://techstore-3fvk.onrender.com/api/v1/products/${name}`) 
+    axios.get(`https://techstore-3fvk.onrender.com/api/v1/products/name/${name}`) 
       .then(response => {
         setItem(response.data);
       })
@@ -16,9 +16,44 @@ function Item({name}) {
       });
   }, []);
 
+    const discount = item.discount ? item.price * (100-item.discount) / 100 : null;
+
     return(<>
-    <div>
-        {item.name}
+    <div className={styles.container}>
+        <div className={styles.item} 
+            style={{ backgroundColor: "rgb(245, 245, 245)" }}>
+            <img className={styles.image} src={item.productImageUrl} alt={item.name} />
+        </div>
+        <div className={styles.itemDescription}>
+            <p className={styles.company}>{item.company}</p>
+            <p className={styles.name}>{item.name}</p>
+            <p className={styles.description}>{item.description}</p>
+            <p className={styles.guarantee}><u>Guarantee of {item.guarantee} month</u></p>
+            <p className={discount ? styles.discounted : styles.price}>{item.price}₼</p>
+            <p className={styles.rating}>Rating: {item.rating}/5.0</p>
+            <div className={styles.ratingLine} style={{width:`${item.rating*2.8}rem`}}></div>
+            <div className={styles.overallRating}></div>
+            <button className={styles.check}>Check reviews</button>
+            {discount && (
+                <>
+                <p className={styles.discount}>{item.discount}%</p>
+                <div className={styles.line}></div>
+                <p className={styles.discountedPrice}>{discount}₼</p>
+                </>
+            )
+            }
+            <button className={styles.buy}>Buy now</button>
+            <button className={styles.cart}>Add to cart</button>
+            <button className={styles.favourite}>Add to favourites</button>
+        </div>
+        <div className={styles.properties}>
+        {Object.entries(item.properties).map(([key, value]) => (
+                <div key={key} className={styles.property}>
+                <strong>{key}:</strong> {value}
+                </div>
+            ))}      
+
+        </div>
     </div>
 
     
