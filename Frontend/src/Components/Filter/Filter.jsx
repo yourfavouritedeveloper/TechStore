e7e8e38dc5de4471e71e15f87448998f5731ac0e
nihadmammadov <nihad.mammadov.16@gmail.com>
2025-08-29 13:56:04 +0400

    Product page UPDATED

--- Frontend/src/Components/Filter/Filter.jsx
+++ Frontend/src/Components/Filter/Filter.jsx
@@ -190,9 +190,7 @@ const displayItems = filteredItems.length ? filteredItems : [];
                       <input type="checkbox" name="more" />
                       <span className={styles.moreSpan}>More</span>
                       <svg xmlns="http://www.w3.org/2000/svg"
-                         height="28px" 
                          viewBox="0 -960 960 960" 
-                         width="28px"
                           fill="#ffffffff">
                       <path d="M400-280v-400l200 200-200 200Z"/></svg>
 
