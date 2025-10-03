783bc4db3721d3422fa41f99e92fbb6b2d7b40bd
gofuuse <nihad.mammadov.16@gmail.com>
2025-10-03 10:47:29 +0400

    Comment Table UPDATED

--- Backend/src/main/java/com/tech/store/model/dto/CommentSummaryDto.java
+++ Backend/src/main/java/com/tech/store/model/dto/CommentSummaryDto.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -14,6 +16,10 @@ public class CommentSummaryDto {
     private String comment;
 
     private AccountSummaryDto fromAccount;
+
+    private Integer like;
+
+    private BigDecimal rate;
 
     private AccountSummaryDto toAccount;
 
