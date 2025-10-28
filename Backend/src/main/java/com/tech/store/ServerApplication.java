0a188200f77cd68b3b1200f9c2e02c9ffac218b0
nihadmammadov <nihad.mammadov.16@gmail.com>
2025-10-28 18:18:13 +0400

    Refresh Token UPDATED

--- Backend/src/main/java/com/tech/store/ServerApplication.java
+++ Backend/src/main/java/com/tech/store/ServerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
-@EnableKafka
 public class ServerApplication {
 
     public static void main(String[] args) {
